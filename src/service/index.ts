7b11798ec5972153c9412a072cdef8d6e9d0652d
Angga7Togk <kiplihode123321@gmail.com>
2025-07-26 01:59:51 +0700

    feat: added service for logic code

---
+++ src/service/index.ts
@@ -0,0 +1,11 @@
+export type Fn = (...args: any[]) => any;
+export type ServiceFactory<TDeps extends object = object, TReturn extends object = object> = (
+  deps: TDeps
+) => TReturn;
+
+export function defineService<
+  TDeps extends object,
+  TReturn extends Record<string, Fn>
+>(factory: ServiceFactory<TDeps, TReturn>): ServiceFactory<TDeps, TReturn> {
+  return factory;
+}
