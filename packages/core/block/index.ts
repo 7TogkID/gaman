c4b00d87908a801cad48a6d28b584b703834e4d6
Angga7Togk <kiplihode123321@gmail.com>
2025-07-28 22:37:07 +0700

    feat(core): update injected dependencies for routes, and services, service(...deps, ...services) and routes(...deps, ...services)

--- packages/core/block/index.ts
+++ packages/core/block/index.ts
@@ -1,107 +1,128 @@
-import { HttpException } from "../error";
-import { RoutesFactory } from "../routes";
-import { ServiceFactory } from "../service";
+import { Priority } from '@gaman/common/utils/priority';
+import { RoutesFactory } from '../routes';
+import { ServiceFactory } from '../service';
 import type {
-  AppConfig,
-  Context,
-  Handler,
-  NextResponse,
-  Priority,
-  RoutesDefinition,
-} from "../types";
+	AppConfig,
+	Context,
+	Handler,
+	NextResponse,
+	RoutesDefinition,
+} from '../types';
+import { HttpException } from '@gaman/common/error/http-exception';
 
 /**
  * Represents the structure of a block in the application.
  */
 export interface BlockFactory<A extends AppConfig> {
-  /**
-   * Defines the base path for this block.
-   * All routes within the block are scoped to this path.
-   */
-  path?: string;
+	/**
+	 * Defines the base path for this block.
+	 * All routes within the block are scoped to this path.
+	 */
+	path?: string;
 
-  /**
-   * Array of included middlewares
-   */
-  includes?: Array<Handler<A>>;
+	/**
+	 * Array of included middlewares
+	 */
+	includes?: Array<Handler<A>>;
 
-  /**
-   * Array of block modules
-   */
-  blocks?: Array<Block<A>>;
+	/**
+	 * Array of block modules
+	 */
+	blocks?: Array<Block<A>>;
 
-  /**
-   * Determines the priority of the block.
-   * Higher priorities are processed earlier.
-   */
-  priority?: Priority;
-  /**
-   * Error handler for the block.
-   * This function is called when an error occurs while processing a request.
-   *
-   * @param error - The error object containing details about the error.
-   * @param ctx - The context object representing the current request.
-   * @returns A `NextResponse` to handle the error gracefully.
-   */
-  error?: (
-    error: HttpException,
-    ctx: Context<A>,
-    next: () => NextResponse
-  ) => NextResponse;
+	/**
+	 * Determines the priority of the block.
+	 * Higher priorities are processed earlier.
+	 */
+	priority?: Priority;
+	/**
+	 * Error handler for the block.
+	 * This function is called when an error occurs while processing a request.
+	 *
+	 * @param error - The error object containing details about the error.
+	 * @param ctx - The context object representing the current request.
+	 * @returns A `NextResponse` to handle the error gracefully.
+	 */
+	error?: (
+		error: HttpException,
+		ctx: Context<A>,
+		next: () => NextResponse,
+	) => NextResponse;
 
-  "404"?: Handler<A>;
+	'404'?: Handler<A>;
 
-  services?: Record<string, ServiceFactory<any, any>>;
+	services?: Record<string, ServiceFactory<any, any>>;
 
-  depedencies?: Record<string, any>;
+	depedencies?: Record<string, any>;
 
-  /**
-   * Defines a set of routes for this block.
-   * Each route maps a path to a specific handler.
-   */
-  routes?: Array<RoutesFactory<any, A>>;
+	/**
+	 * Defines a set of routes for this block.
+	 * Each route maps a path to a specific handler.
+	 */
+	routes?: Array<RoutesFactory<any, A>>;
 }
 
 export interface Block<A extends AppConfig> extends BlockFactory<A> {
-  services_useable: Record<string, any>;
-  routes_useable: RoutesDefinition<A>;
+	services_useable: Record<string, any>;
+	routes_useable: RoutesDefinition<A>;
 }
 
 /**
  * Define a new block for routing, middleware, error handling, etc.
  */
 export function defineBlock<A extends AppConfig>(
-  block: BlockFactory<A>
+	block: BlockFactory<A>,
 ): Block<A> {
-  if (!block.depedencies) block.depedencies = {};
+	if (!block.depedencies) block.depedencies = {};
+	if (!block.services) block.services = {};
+  
+	const serviceCache: Record<string, any> = {}; // * service cache biar ga berat karna pakai proxy
 
-  let services_useable: Record<string, any> = {};
-  if (block.services) {
-    for (const [key, factory] of Object.entries(block.services)) {
-      services_useable[key] = factory(block.depedencies || {});
-    }
-  }
+  /**
+   * * Proxy ini adalah Wrapper dinamis jadi bisa ngambil object dinamis mirip lamda lah
+   */
+	const context = new Proxy(
+		{},
+		{
+      // * fungsi get ini seperti saat orang pakai context['service'] nah 'service' ini itu prop
+			get(_, prop: string) {
+        // ? kalau udah ada di cache pakai cache aja!
+				if (prop in serviceCache) return serviceCache[prop];
 
-  let routes_useable: RoutesDefinition<A> = {};
-  if (block.routes) {
-    for (const routeFactory of block.routes) {
-      const routes = routeFactory({
-        ...services_useable,
-        ...block.depedencies,
-      });
-      routes_useable = { ...routes_useable, ...routes };
-    }
-  }
+				if (prop in block.services!) {
+					const factory = block.services![prop];
+					const instance = factory(context);
+					serviceCache[prop] = instance;
+					return instance;
+				}
+        
+				if (prop in block.depedencies!) return block.depedencies![prop];
 
-  return {
-    ...block,
-    services_useable,
-    routes_useable,
-  };
+				return undefined;
+			},
+		},
+	);
+
+  // * register services_useable artinya services yang bakal di pakai
+	const services_useable: Record<string, any> = {};
+	if (block.services) {
+		for (const [key, factory] of Object.entries(block.services)) {
+			services_useable[key] = factory(context);
+		}
+	}
+
+	// * register routes_useable artinya routes yang bakal di pakai
+	let routes_useable: RoutesDefinition<A> = {};
+	if (block.routes) {
+		for (const factory of block.routes) {
+			const routes = factory(context);
+			routes_useable = { ...routes_useable, ...routes };
+		}
+	}
+
+	return {
+		...block,
+		services_useable,
+		routes_useable,
+	};
 }
-
-defineBlock({
-  404: () => {
-    return {};
-  },
-});
