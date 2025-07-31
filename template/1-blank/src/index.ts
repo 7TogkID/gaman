a7e67909c5d4480688f6e26da0a1033d0f489b19
Angga7Togk <kiplihode123321@gmail.com>
2025-07-31 20:52:00 +0700

    feat(template): change template 1

--- template/1-blank/src/index.ts
+++ template/1-blank/src/index.ts
@@ -3,11 +3,10 @@ import mainBlock from './main.block';
 import { TextFormat } from '@gaman/common/utils/textformat';
 
 defineBootstrap(mainBlock, async (app) => {
-	app.setStrict(false) // <-- setStrict(true) if you want use strict mode
+	app.setStrict(false); // <-- setStrict(true) if you want use strict mode
 
-	app.listen(3431, 'localhost', () => {
-		Log.log(
-			`Server is running at ${TextFormat.UNDERLINE}http://localhost:3431${TextFormat.RESET}`,
-		);
-	});
+	await app.listen(3431);
+	Log.log(
+		`Server is running at ${TextFormat.UNDERLINE}http://localhost:3431${TextFormat.RESET}`,
+	);
 });
