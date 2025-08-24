794176a787cb048ad5dee161aa726b1fc6772dac
Angga7Togk <kiplihode123321@gmail.com>
2025-08-24 21:11:57 +0700

    [handler] remove handler identity

--- packages/core/handler/index.ts
+++ packages/core/handler/index.ts
@@ -1,33 +1,16 @@
 import { RequestHandler } from '@gaman/core/types';
 import {
 	IS_COMPOSE_HANDLER_FACTORY,
-	IS_REQUEST_HANDLER,
 } from '@gaman/common/contants';
 
 export type HandlerFactory = () => Record<string, RequestHandler>;
 
 export function composeHandler(factory: HandlerFactory): HandlerFactory {
-	const newFactory = () => {
-		const handlers = factory();
-
-		// ? taruh identity bahwa dia itu handler ke semua method
-		for (const value of Object.values(handlers)) {
-			if (typeof value === 'function') {
-				Object.defineProperty(value, IS_REQUEST_HANDLER, {
-					value: true,
-					writable: false,
-					enumerable: false,
-				});
-			}
-		}
-
-		return handlers;
-	};
-	Object.defineProperty(newFactory, IS_COMPOSE_HANDLER_FACTORY, {
+	Object.defineProperty(factory, IS_COMPOSE_HANDLER_FACTORY, {
 		value: true,
 		writable: false,
 		enumerable: false,
 	});
 
-	return newFactory;
+	return factory;
 }
