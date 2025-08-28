0730d5eed6cf14a197d696b51351f6808b5cb0f8
Angga7Togk <kiplihode123321@gmail.com>
2025-08-28 22:47:06 +0700

    [core] add autoComposeExceptionHandler

--- packages/core/exception/index.ts
+++ packages/core/exception/index.ts
@@ -1,4 +1,14 @@
+import { registerExceptions } from '@gaman/core/registry.js';
+
 export type ExceptionHandler = (error: Error) => any;
+
+export function autoComposeExceptionHandler(
+	cb: ExceptionHandler,
+): ExceptionHandler {
+	const handler = composeExceptionHandler(cb);
+	registerExceptions(handler);
+	return handler;
+}
 
 export function composeExceptionHandler(
 	cb: ExceptionHandler,
