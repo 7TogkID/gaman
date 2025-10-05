843a613aa46d870a8a0ddaeb92f927a110b30cde
Angga7Togk <kiplihode123321@gmail.com>
2025-10-05 22:42:33 +0700

    [feat][store] add undefined result on get method

--- packages/common/types/store.types.ts
+++ packages/common/types/store.types.ts
@@ -1,5 +1,5 @@
 export interface Store<K = string, V = any> {
-	get(key: K): V | Promise<V>;
+	get(key: K): V | undefined | Promise<V | undefined>;
 	set(key: K, value: V, ttl?: number): void | Promise<void>;
 	has(key: K): boolean | Promise<boolean>;
 	delete(key: K): void | Promise<void>;
