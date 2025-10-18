d0d44aac7283114aa58bec2aad3ea28fdcbbfb82
fiandev <fiandev1234@gmail.com>
2025-10-18 19:27:40 +0700

    feat(jwt): add jwt middleware
    Adds a new JWT middleware to the project. This middleware provides functionality for signing and verifying JWTs, and can optionally handle JWTs from cookies or headers. It also includes basic documentation for usage and options.

--- plugins/jwt/index.ts
+++ plugins/jwt/index.ts
@@ -1,5 +1,4 @@
 import {
-	Context,
 	DefaultMiddlewareOptions,
 	sign,
 	verify,
@@ -8,11 +7,36 @@ import {
 import { composeMiddleware } from '@gaman/core';
 import { Response } from '@gaman/core/response.js';
 
+/**
+ * Represents the options for the JWT middleware.
+ */
 export interface JwtOptions extends DefaultMiddlewareOptions {
+	/**
+	 * The secret key used to sign and verify the JWT.
+	 */
 	secret: string;
+	/**
+	 * The name of the cookie to store the JWT in.
+	 * If this is provided, the middleware will look for the JWT in the cookies.
+	 */
 	cookie?: string;
+	/**
+	 * The name of the header to look for the JWT in.
+	 * If this is provided, the middleware will look for the JWT in the headers.
+	 * The header should be in the format "Bearer <token>".
+	 */
 	header?: string;
+	/**
+	 * The expiration time for the JWT.
+	 * This can be a string (e.g., "1h", "1d") or a number in seconds.
+	 * @default 6000
+	 */
 	expiresIn?: string | number;
+	/**
+	 * Whether the JWT is required for the route.
+	 * If this is true and the JWT is not present or invalid, the middleware will return a 401 Unauthorized response.
+	 * @default false
+	 */
 	required?: boolean;
 }
 
@@ -30,6 +54,32 @@ declare global {
 
 const DEFAULT_JWT_EXPIRATION = 6000;
 
+/**
+ * A middleware for handling JSON Web Tokens (JWT).
+ * This middleware adds a `jwt` object to the context, which contains methods for signing and verifying JWTs.
+ * It also checks for a JWT in the cookies or headers and populates `ctx.jwt.user` with the decoded payload.
+ *
+ * @param options - The options for the JWT middleware.
+ * @returns A middleware function.
+ *
+ * @example
+ * ```ts
+ * import { Gaman } from '@gaman/core';
+ * import { jwt } from '@gaman/jwt';
+ *
+ * const app = new Gaman();
+ *
+ * app.use(jwt({ secret: 'your-secret-key' }));
+ *
+ * app.get('/profile', (ctx) => {
+ *   if (ctx.jwt.user) {
+ *     return ctx.json({ user: ctx.jwt.user });
+ *   } else {
+ *     return ctx.json({ message: 'Unauthorized' }, 401);
+ *   }
+ * });
+ * ```
+ */
 export const jwt = (options: JwtOptions) => {
 	const signToken = (payload: object, expiresIn?: string | number) => {
 		const expire = expiresIn || options.expiresIn || DEFAULT_JWT_EXPIRATION;
